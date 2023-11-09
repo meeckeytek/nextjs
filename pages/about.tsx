837fe5b98754aa6302d624298718dc25e384eaf4
Anuoluwapo Okanlawon <tunsabaze01@gmail.com>
2023-11-09 07:22:02 +0000

    Project commit

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import NavLink from "next/link"
+
+const About = () => {
+
+
+    return (<>
+    
+    <NavLink href='/test-page-welcome/'>Test Page</NavLink>
+    
+    <h1>Welcome Page</h1><h1>About Page</h1></>)
+}
+
+
+export default About
